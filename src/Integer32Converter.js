c402a2a1c23e9461b594e2866dbe41ccd69e026f
Jeremy Gray <jeremy@jeremygray.ca>
2010-03-16 14:25:37 -0700

    Whitespace fixes (tabs to spaces.)

--- src/Integer32Converter.js
+++ src/Integer32Converter.js
@@ -1,12 +1,12 @@
 ﻿tpzBase32 = {
-	// Port safety test of encodingAlphabet
-	encodingAlphabet: "ybndrfg8ejkmcpqxot1uwisza345h769",
-	quintetMask: 31
+    // Port safety test of encodingAlphabet
+    encodingAlphabet: "ybndrfg8ejkmcpqxot1uwisza345h769",
+    quintetMask: 31
 };
 
 tpzBase32.encodeQuintet = function (quintet) {
     // Add guard clauses and their tests!!! including a negative range check
-	
+
     if (quintet > 31) {
         throw {
             name: "argument out of range",
@@ -20,38 +20,38 @@ tpzBase32.encodeQuintet = function (quintet) {
 tpzBase32.normalizeToTpzBase32Alphabet = function (encodedCharacter) {
     // Add guard clauses and their tests!!!
 
-	if (encodedCharacter === '0')
-	{
-		return 'o';
-	}
-	if (encodedCharacter === 'l' || encodedCharacter === 'L')
-	{
-		return '1';
-	}
-	if (encodedCharacter === '2')
-	{
-		return 'z';
-	}
-	if (encodedCharacter === 'v' || encodedCharacter === 'V')
-	{
-		return 'u';
-	}
+    if (encodedCharacter === '0')
+    {
+        return 'o';
+    }
+    if (encodedCharacter === 'l' || encodedCharacter === 'L')
+    {
+        return '1';
+    }
+    if (encodedCharacter === '2')
+    {
+        return 'z';
+    }
+    if (encodedCharacter === 'v' || encodedCharacter === 'V')
+    {
+        return 'u';
+    }
 
-	return encodedCharacter.toLowerCase();
+    return encodedCharacter.toLowerCase();
 };
 
 tpzBase32.decodeToQuintet = function (encodedCharacter) {
     // Add guard clauses and their tests!!!
 
-	var normalizedEncodedCharacter = tpzBase32.normalizeToTpzBase32Alphabet(encodedCharacter);
-	var index = tpzBase32.encodingAlphabet.indexOf(normalizedEncodedCharacter);
+    var normalizedEncodedCharacter = tpzBase32.normalizeToTpzBase32Alphabet(encodedCharacter);
+    var index = tpzBase32.encodingAlphabet.indexOf(normalizedEncodedCharacter);
 
-	// if (index === -1)
-	// {
-		// throw new ArgumentOutOfRangeException("input");
-	// }
+    // if (index === -1)
+    // {
+        // throw new ArgumentOutOfRangeException("input");
+    // }
 
-	return index;
+    return index;
 };
 
 tpzBase32.Integer32Converter = function () {};
@@ -96,23 +96,23 @@ tpzBase32.Integer32Converter.prototype.encode = function (input, includePadding)
 
 tpzBase32.Integer32Converter.prototype.decode = function (input) {
     // Add guard clauses and their tests!!!
-	
-	// if (input == null)
-	// {
-		// throw new ArgumentNullException("input");
-	// }
 
-	// if (input.Length == 0 || input.Length > 7)
-	// {
-		// throw new ArgumentOutOfRangeException("input", input,
-											  // "Must be string of between 1 and 7 characters inclusive.");
-	// }
+    // if (input == null)
+    // {
+        // throw new ArgumentNullException("input");
+    // }
 
-	var i;
-	var result = 0;
-	for (i = 0; i < input.length; i++) {
-		var quintet = tpzBase32.decodeToQuintet(input.charAt(i));
-		result |= quintet << (5 * i);
-	}
-	return result;
+    // if (input.Length == 0 || input.Length > 7)
+    // {
+        // throw new ArgumentOutOfRangeException("input", input,
+                                              // "Must be string of between 1 and 7 characters inclusive.");
+    // }
+
+    var i;
+    var result = 0;
+    for (i = 0; i < input.length; i++) {
+        var quintet = tpzBase32.decodeToQuintet(input.charAt(i));
+        result |= quintet << (5 * i);
+    }
+    return result;
 };
